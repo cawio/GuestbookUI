e691cc8d806b790d44b14804ac9cad0b07fe9c48
Simon <sberner9@gmail.com>
2024-03-15 09:25:30 +0100

    test: :white_check_mark: Add unit tests for EntryService

--- src/app/services/entry.service.spec.ts
+++ src/app/services/entry.service.spec.ts
@@ -1,16 +1,115 @@
+import {
+    HttpClientTestingModule,
+    HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+
+import { EntryDTO } from '@dtos/EntryDTO';
 
 import { EntryService } from './entry.service';
 
 describe('EntryService', () => {
     let service: EntryService;
+    let httpMock: HttpTestingController;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [EntryService],
+        });
         service = TestBed.inject(EntryService);
+        httpMock = TestBed.inject(HttpTestingController);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should fetch entries', () => {
+        service.getEntries().subscribe((entries) => {
+            expect(entries).toBeTruthy();
+            expect(entries.length).toBe(6);
+            expect(entries[0].id).toBe('1');
+            expect(typeof entries).toBe(typeof Array<EntryDTO>);
+        });
+
+        const request = httpMock.expectOne(`${service.apiUrl}/entries`);
+        expect(request.request.method).toBe('GET');
+        request.flush([
+            {
+                id: '1',
+                title: 'Mock Entry 3',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '2',
+                title: 'Mock Entry 2',
+                message:
+                    'This is another mock entry message that is longer than 100 characters. We need to test if the grid will display this entry in two rows.',
+                creatorName: 'Flynn Rider',
+                creationTime: new Date(),
+            },
+            {
+                id: '3',
+                title: 'Mock Entry 1. With a long title. We need to be larger 30',
+                message: 'This is a mock entry message. With a long title.',
+                creatorName: 'Dr. Dr. med. Flyman',
+                creationTime: new Date(),
+            },
+            {
+                id: '4',
+                title: 'Mock Entry 4',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '5',
+                title: 'Mock Entry 5',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '6',
+                title: 'Mock Entry 6',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+        ]);
+    });
+
+    it('should create an entry', () => {
+        const entry: EntryDTO = {
+            id: '1',
+            title: 'Mock Entry 3',
+            message: 'Short message.',
+            creatorName: 'John Doe',
+            creationTime: new Date(),
+        };
+
+        service.createEntry(entry).subscribe((response) => {
+            expect(response).toBeTruthy();
+            expect(response.id).toBe('1');
+        });
+
+        const request = httpMock.expectOne(`${service.apiUrl}/entries`);
+        expect(request.request.method).toBe('POST');
+        request.flush(entry);
+    });
+
+    it('should delete an entry', () => {
+        const idToDelete = '1';
+
+        service.deleteEntry(idToDelete).subscribe((response) => {
+            expect(response).toBeTruthy();
+        });
+
+        const request = httpMock.expectOne(`${service.apiUrl}/entries/1`);
+        expect(request.request.method).toBe('DELETE');
+        request.flush(idToDelete);
     });
 });
--- src/app/services/entry.service.ts
+++ src/app/services/entry.service.ts
@@ -9,11 +9,127 @@ import { environment } from '@env/environment';
     providedIn: 'root',
 })
 export class EntryService {
-    private readonly apiUrl = environment.apiUrl;
+    readonly apiUrl = environment.apiUrl;
 
     constructor(private http: HttpClient) {}
 
     getEntries(): Observable<EntryDTO[]> {
+        const mockEntries: EntryDTO[] = [
+            {
+                id: '1',
+                title: 'Mock Entry 3',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '2',
+                title: 'Mock Entry 2',
+                message:
+                    'This is another mock entry message that is longer than 100 characters. We need to test if the grid will display this entry in two rows.',
+                creatorName: 'Flynn Rider',
+                creationTime: new Date(),
+            },
+            {
+                id: '3',
+                title: 'Mock Entry 1. With a long title. We need to be larger 30',
+                message: 'This is a mock entry message. With a long title.',
+                creatorName: 'Dr. Dr. med. Flyman',
+                creationTime: new Date(),
+            },
+            {
+                id: '4',
+                title: 'Mock Entry 4',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '5',
+                title: 'Mock Entry 5',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '6',
+                title: 'Mock Entry 6',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '7',
+                title: 'Mock Entry 7',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '8',
+                title: 'Mock Entry 8',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '9',
+                title: 'Mock Entry 9',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '10',
+                title: 'Mock Entry 10',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '11',
+                title: 'Mock Entry 11',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '12',
+                title: 'Mock Entry 12',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '13',
+                title: 'Mock Entry 13',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '14',
+                title: 'Mock Entry 14',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '15',
+                title: 'Mock Entry 15',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+            {
+                id: '16',
+                title: 'Mock Entry 16',
+                message: 'Short message.',
+                creatorName: 'John Doe',
+                creationTime: new Date(),
+            },
+        ];
+
         return of(mockEntries).pipe(delay(1000));
     }
 
@@ -21,36 +137,11 @@ export class EntryService {
         return of(entry).pipe(delay(1000));
     }
 
-    deleteEntry(id: string): Observable<null> {
-        if (!id) {
-            return of(null);
+    deleteEntry(id: string): Observable<boolean> {
+        if (id === '1') {
+            return of(true).pipe(delay(1000));
         }
 
-        return of(null).pipe(delay(1000));
+        return of(true).pipe(delay(1000));
     }
 }
-
-const mockEntries: EntryDTO[] = [
-    {
-        id: 'JKLÖsjlih',
-        title: 'Mock Entry 3',
-        message: 'Short message.',
-        creatorName: 'John Doe',
-        creationTime: new Date(),
-    },
-    {
-        id: 'JKhjGGlih',
-        title: 'Mock Entry 2',
-        message:
-            'This is another mock entry message that is longer than 100 characters. We need to test if the grid will display this entry in two rows.',
-        creatorName: 'Flynn Rider',
-        creationTime: new Date(),
-    },
-    {
-        id: 'JKkljulih',
-        title: 'Mock Entry 1. With a long title. We need to be larger 30',
-        message: 'This is a mock entry message. With a long title.',
-        creatorName: 'Dr. Dr. med. Flyman',
-        creationTime: new Date(),
-    },
-];
