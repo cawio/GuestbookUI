bd36f46ba106fca0d23e1b6d77cddaa017268dac
Simon <sberner9@gmail.com>
2024-03-18 08:11:40 +0100

    feat: :recycle: Refactor login component and update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
         component: GuestbookGridComponent,
     },
     {
-        path: 'admin',
+        path: 'dashboard',
         canActivate: [authGuard],
         component: AdminDashboardComponent,
     },
--- src/app/features/authentication/login/login.component.ts
+++ src/app/features/authentication/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 
 import { AppStore } from '@stores/app.store';
@@ -6,17 +11,35 @@ import { AppStore } from '@stores/app.store';
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [],
+    imports: [
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+    ],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
     private readonly appStore = inject(AppStore);
+    loginForm: FormGroup;
 
-    constructor(private router: Router) {}
+    constructor(
+        private router: Router,
+        private fb: FormBuilder
+    ) {
+        this.loginForm = this.fb.group({
+            username: [''],
+            password: [''],
+        });
+    }
 
-    ngOnInit(): void {
+    onSubmit(): void {
+        console.log(this.loginForm.value);
+
+        // TODO: Implement login logic
         this.appStore.login();
-        this.router.navigate(['/admin']);
+        this.router.navigate(['/dashboard']);
     }
 }
